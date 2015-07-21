8d754bcd9522297625986671c7406a967f3022c7
acpride <acpride@gmail.com>
2015-07-21 12:05:08 +0200

    Primera pregunta

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+//añadimos controller
+var quizController = require('../controllers/quiz_controller');
+
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
+
+router.get('/quizzes/question', quizController.question);
+router.get('/quizzes/answer', quizController.answer);
 
 module.exports = router;
