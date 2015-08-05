a00fd32b828107bc6800cf70e737ae64d1bc0e8a
acpride <acpride@gmail.com>
2015-08-05 12:27:27 +0200

    Moderación de comentarios

--- routes/index.js
+++ routes/index.js
@@ -12,6 +12,7 @@ router.get('/', function(req, res) {
 
 // Autoload de comandos con :quizId
 router.param('quizId', quizController.load);  // autoload :quizId
+router.param('commentId', commentController.load);  // autoload :commentId
 
 
 // Definición de rutas de sesion
@@ -41,5 +42,6 @@ router.delete('/quizzes/:quizId(\\d+)',     sessionController.loginRequired, qui
 // Definición de rutas de /quizzes/comments
 router.get('/quizzes/:quizId(\\d+)/comments/new',		commentController.new);
 router.post('/quizzes/:quizId(\\d+)/comments',			commentController.create);
+router.get('/quizzes/:quizId(\\d+)/comments/:commentId(\\d+)/publish', sessionController.loginRequired, commentController.publish);
 
 module.exports = router;
